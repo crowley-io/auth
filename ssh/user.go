2375d728db9af751d16a6eba3884f6e3a6fd54b8
Thomas LE ROUX <github@november-eleven.fr>
2015-05-20 18:06:22 +0200

    docs(ssh): User struct

--- ssh/user.go
+++ ssh/user.go
@@ -12,14 +12,17 @@ type User struct {
 	key  []byte
 }
 
+// Username from incoming ssh connection
 func (u *User) Name() string {
 	return u.name
 }
 
+// SSH Key in base64 used by incoming ssh connection
 func (u *User) Key() string {
 	return base64.StdEncoding.EncodeToString(u.key)
 }
 
+// SSH Key's fingerprint from incoming ssh connection
 func (u *User) Fingerprint() string {
 	h := md5.Sum(u.key)
 	f := fmt.Sprintf("% x", h)
