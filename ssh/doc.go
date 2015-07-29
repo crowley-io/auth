a8b67c431760e6c5c5eaf27ec97f199d74518fd7
Thomas LE ROUX <thomas@november-eleven.fr>
2015-07-30 01:00:32 +0200

    docs(ssh): Update

--- ssh/doc.go
+++ ssh/doc.go
@@ -1,4 +1,4 @@
-// Package ssh provides a minimal framework for using ssh as a secure webapp's signing mecanism.
+// Package ssh provides a minimal framework for using SSH as a secure webapp's signing mecanism.
 //
 // This framework is inspired from this article: https://vtllf.org/blog/ssh-web-sign-in
 //
