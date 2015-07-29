7e7f2662998be0c952aea324093065d449aa6de7
Thomas LE ROUX <thomas@november-eleven.fr>
2015-07-30 00:45:15 +0200

    chore: Remove errors

--- mfa/errors.go
+++
@@ -1,9 +0,0 @@
-package mfa
-
-import "errors"
-
-var (
-	ErrCreateRandSecret = errors.New("An error has occurred while creating random secret key")
-	ErrSecretURIFormat  = errors.New("An error has occurred while creating secret URI")
-	ErrQREncoding       = errors.New("An error has occurred while encoding QR code")
-)
