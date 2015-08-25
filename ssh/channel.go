ab981995ab648877297a76bb4c331880e234e825
Thomas LE ROUX <thomas@november-eleven.fr>
2015-08-25 16:48:24 +0200

    style(ssh): fix

--- ssh/channel.go
+++ ssh/channel.go
@@ -3,7 +3,7 @@ package ssh
 import "golang.org/x/crypto/ssh"
 
 //
-// Wrapper for ssh.Channel to avoid package name conflict with golang.org/x/crypto/ssh 
+// Wrapper for ssh.Channel to avoid package name conflict with golang.org/x/crypto/ssh
 // and github.com/crowley-io/auth/ssh
 //
 type Channel struct {
